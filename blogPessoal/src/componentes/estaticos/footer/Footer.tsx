5f99a9ad91631c38870812b8f4e170985d503a9c
BiancaMelquiades <biancamelquiadesbia@gmail.com>
2023-05-18 22:47:53 -0300

    alteração de código e cadastro de usuário

--- blogPessoal/src/componentes/estaticos/footer/Footer.tsx
+++ blogPessoal/src/componentes/estaticos/footer/Footer.tsx
@@ -1,39 +1,38 @@
-import React from 'react';
+import React from "react";
 import InstagramIcon from '@material-ui/icons/Instagram';
-import GitHubIcon from '@mui/icons-material/GitHub';
+import FacebookIcon from '@material-ui/icons/Facebook';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
-import {Typography, Grid } from '@material-ui/core';
-import {Box} from '@mui/material';
-import "./Footer.css";
+import { Typography, Grid } from '@material-ui/core';
+import { Box } from '@mui/material';
 
 function Footer() {
     return (
         <>
             <Grid container direction="row" justifyContent="center" alignItems="center">
                 <Grid alignItems="center" item xs={12}>
-                    <Box className='box1'>
+                    <Box style={{ backgroundColor: "#595b5a", height: "120px" }}>
                         <Box paddingTop={1} display="flex" alignItems="center" justifyContent="center">
-                            <Typography variant="h5" align="center" gutterBottom className='textos'>Siga-nos nas redes sociais </Typography>
+                            <Typography variant="h6" align="center" gutterBottom style={{ color: "white" }}>Siga-nos nas redes sociais </Typography>
                         </Box>
                         <Box display="flex" alignItems="center" justifyContent="center">
-                            <a href="https://github.com/Bruna-alencar" target="_blank">
-                                <GitHubIcon className='redes'/>
+                            <a href="https://www.facebook.com/generationbrasil" target="_blank">
+                                <FacebookIcon style={{ fontSize: 60, color: "white", width:"40px" }} />
                             </a>
-                            <a href="https://www.instagram.com/invites/contact/?i=10d7vje6lom4b&utm_content=5g0g05" target="_blank">
-                                <InstagramIcon className='redes'/>
+                            <a href="https://www.instagram.com/generationbrasil/" target="_blank">
+                                <InstagramIcon style={{ fontSize: 60, color: "white", width:"40px"}} />
                             </a>
-                            <a href="https://www.linkedin.com/in/bruna-santos-alencar-565a55193/" target="_blank">
-                                <LinkedInIcon className='redes'/>
+                            <a href="https://www.linkedin.com/school/generationbrasil/" target="_blank">
+                                <LinkedInIcon style={{ fontSize: 60, color: "white", width:"40px" }} />
                             </a>
                         </Box>
                     </Box>
-                    <Box className='box2'>
+                    <Box style={{ backgroundColor: "#4c4d4c", height: "60px" }}>
                         <Box paddingTop={1}>
-                            <Typography variant="subtitle2" align="center" gutterBottom className='textos' >© 2023 Copyright:</Typography>
+                            <Typography variant="subtitle2" align="center" gutterBottom style={{ color: "white" }} >© 2023 Copyright:</Typography>
                         </Box>
                         <Box>
-                            <a target="_blank" href="https://brasil.generation.org">
-                                <Typography variant="subtitle2" gutterBottom className='textos' align="center">brasil.generation.org</Typography>
+                            <a target="_blank" style={{textDecoration: "none"}} href="https://brasil.generation.org">
+                                <Typography variant="subtitle2" gutterBottom style={{ color: "white" }} align="center">brasil.generation.org</Typography>
                             </a>
                         </Box>
                     </Box>
@@ -41,6 +40,6 @@ function Footer() {
             </Grid>
         </>
     )
-}
+};
 
 export default Footer;
--- blogPessoal/src/componentes/estaticos/navbar/Navbar.tsx
+++ blogPessoal/src/componentes/estaticos/navbar/Navbar.tsx
@@ -1,55 +1,186 @@
-import React from 'react';
-import { AppBar, Grid, Toolbar, Typography } from '@material-ui/core';
-import {Box} from '@mui/material';
+import React from "react";
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+import Diversity1Icon from '@mui/icons-material/Diversity1';
 import { Link } from 'react-router-dom';
-import "./Navbar.css"
+
+
+const pages = ['Início', 'Postagens', 'Temas', 'Cadastrar Tema'];
+// const settings = ['Perfil', 'Conta', 'Visão Geral', 'Sair'];
+// Breve: fazer o mesmo pra pages quando for fazer a rota.
+const settings = [
+    {
+        nome: 'Perfil',
+        link: '/'
+    },
+    {
+        nome: 'Conta',
+        link: '/'
+    },
+    {
+        nome: 'Sair',
+        link: '/login'
+    },
+]
 
 function Navbar() {
+    const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
+    const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
+    
+
+    const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorElNav(event.currentTarget);
+    };
+    const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorElUser(event.currentTarget);
+    };
+
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
+    };
+
+    const handleCloseUserMenu = () => {
+        setAnchorElUser(null);
+    };
+
     return (
         <>
-            <AppBar position="static">
-                <Toolbar variant="dense">
-                    <Box className='cursor'>
-                        <Typography variant="h5" color="inherit">
-                            BlogPessoal
+            <AppBar position="static" style={{backgroundColor: "#595b5a"}}>
+                <Container maxWidth="xl">
+                    <Toolbar disableGutters>
+                        <Diversity1Icon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+                        <Typography
+                            variant="h6"
+                            noWrap
+                            component="a"
+                            href="/"
+                            sx={{
+                                mr: 2,
+                                display: { xs: 'none', md: 'flex' },
+                                fontFamily: 'monospace',
+                                fontWeight: 700,
+                                letterSpacing: '.3rem',
+                                color: 'inherit',
+                                textDecoration: 'none',
+                            }}
+                        >
+                            Blog Pessoal
                         </Typography>
-                    </Box>
-                    
-                    <Box display="flex" justifyContent="start">
-                        <Box mx={1} className='cursor'>
-                            <Typography variant="h6" color="inherit">
-                                Home
-                            </Typography>
-                        </Box>
-                        <Box mx={1} className='cursor'>
-                            <Typography variant="h6" color="inherit">
-                                Postagens
-                            </Typography>
-                        </Box>
-                        <Box mx={1} className='cursor'>
-                            <Typography variant="h6" color="inherit">
-                                Temas
-                            </Typography>
-                        </Box>
-                        <Box mx={1} className='cursor'>
-                            <Typography variant="h6" color="inherit">
-                                Cadastrar Tema
-                            </Typography>
-                        </Box>
-                        <Link to="/login" className="text-decorator-none">
-                            <Box mx={1} className='cursor'>
-                                <Typography variant="h6" color="inherit">
-                                    Login
-                                </Typography>
-                            </Box>
-                        </Link>
-                        
-                    </Box>
 
-                </Toolbar>
+                        <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+                            <IconButton
+                                size="large"
+                                aria-label="account of current user"
+                                aria-controls="menu-appbar"
+                                aria-haspopup="true"
+                                onClick={handleOpenNavMenu}
+                                color="inherit"
+                            >
+                                <MenuIcon />
+                            </IconButton>
+                            <Menu
+                                id="menu-appbar"
+                                anchorEl={anchorElNav}
+                                anchorOrigin={{
+                                    vertical: 'bottom',
+                                    horizontal: 'left',
+                                }}
+                                keepMounted
+                                transformOrigin={{
+                                    vertical: 'top',
+                                    horizontal: 'left',
+                                }}
+                                open={Boolean(anchorElNav)}
+                                onClose={handleCloseNavMenu}
+                                sx={{
+                                    display: { xs: 'block', md: 'none' },
+                                }}
+                            >
+                                {pages.map((page) => (
+                                    <MenuItem key={page} style={{display: "block", margin: "10px"}} onClick={handleCloseNavMenu}>
+                                        <Typography textAlign="center">{page}</Typography>
+                                    </MenuItem>
+                                ))}
+                            </Menu>
+                        </Box>
+                    
+                        <Typography
+                            variant="h5"
+                            noWrap
+                            component="a"
+                            href=""
+                            sx={{
+                                mr: 2,
+                                display: { xs: 'flex', md: 'none' },
+                                flexGrow: 1,
+                                fontFamily: 'monospace',
+                                fontWeight: 700,
+                                letterSpacing: '.3rem',
+                                color: 'inherit',
+                                textDecoration: 'none',
+                            }}
+                        >
+                            Blog Pessoal
+                        </Typography>
+                        <Box gap={2} sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+                            {pages.map((page) => (
+                                <Button
+                                    key={page}
+                                    onClick={handleCloseNavMenu}
+                                    sx={{ my: 2, color: '#ffff', display: 'block' }}
+                                    style={{color:"#ffff"}}
+                                >
+                                    {page}
+                                </Button>
+                            ))}
+                        </Box>
+
+                        <Box sx={{ flexGrow: 0 }}>
+                            <Tooltip title="Open settings">
+                                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                                <Avatar alt="Foto Perfil" src="https://media.licdn.com/dms/image/C4D03AQFdlK6-d7blgg/profile-displayphoto-shrink_800_800/0/1652486506638?e=1689811200&v=beta&t=mj2Tz4egbmgZOC-MT9aAbKR8e2uZFcCjwUfy0vESZ7I" />
+                                </IconButton>
+                            </Tooltip>
+                            <Menu
+                                sx={{ mt: '45px' }}
+                                id="menu-appbar"
+                                anchorEl={anchorElUser}
+                                anchorOrigin={{
+                                    vertical: 'top',
+                                    horizontal: 'right',
+                                }}
+                                keepMounted
+                                transformOrigin={{
+                                    vertical: 'top',
+                                    horizontal: 'right',
+                                }}
+                                open={Boolean(anchorElUser)}
+                                onClose={handleCloseUserMenu}
+                            >
+                                {settings.map((setting) => (
+                                    <MenuItem key={setting.nome} style={{display: "block", margin: "10px"}} onClick={handleCloseUserMenu}>
+                                        <Link to={setting.link} style={{ textDecoration: 'none' }}>
+                                        <Typography textAlign="center">{setting.nome}</Typography>
+                                        </Link>
+                                    </MenuItem>
+                                ))}
+                            </Menu>
+                        </Box>
+                    </Toolbar>
+                </Container>
             </AppBar>
         </>
     )
-}
+};
 
 export default Navbar;
